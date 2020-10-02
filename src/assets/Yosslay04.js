6c36acb6cb03cff0e01cd1a40a3a6703fccad1b7
Yosslay04 <57569710+Yosslay04@users.noreply.github.com>
2020-10-02 15:39:06 -0500

    Create Yosslay04.js

---
+++ src/assets/Yosslay04.js
@@ -0,0 +1,9 @@
+ export const Yosslay04 = {
+            "name": "Yosslay Cervantes",
+            "gif": "url of you favorite gif", // using gif link on https://giphy.com/
+            "skills": "languages you know or want to learn more of during Hacktoberfest",
+            "github": "https://github.com/Yosslay04"
+        }
+        export default Yosslay04
+        
+        export * from './thedjziegler'
